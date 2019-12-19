5cdd2d15a29eab17068fc51829ac387887cdfbef
Pedro Ivo de Faria Ramos <pedro.ramos@embraer.com.br>
2019-12-19 13:00:24 -0300

    Finalização Python3 Avançado!

--- Python3/OO2/modelo.py
+++ Python3/OO2/modelo.py
@@ -1,3 +1,4 @@
+#################################Classe Mãe###################################################
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -19,34 +20,80 @@ class Programa:
     def nome(self, nome):
         self._nome = nome.title()
 
+    def imprime(self):
+        print(f"{self._nome} - {self.ano} - {self._likes} Likes")
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
+
+#######################Classes filhas de Programa##########################################
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes"
+
     def valores_diferentes_para_filme(self):
         pass
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes"
+
+
+#Playlist(list)##Uso como se fosse list, list é um tipo, que é interavel
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+
+    def __len__(self):
+        return len(self._programas)
 
 
 
-
-
-##################MAIN DO CODIGO, ONDE OS TESTES DAS CLASSES DO CURSO DE OO2 IRAO OCORRER!! ##############
-
-
+######################MAIN DO CODIGO, ONDE OS TESTES DAS CLASSES DO CURSO DE OO2 IRAO OCORRER!!#########################
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.nome = 'vingadores - guerra inifinita parte 2'
-vingadores.dar_likes()
-print(f"{vingadores.nome} - {vingadores.duracao} : {vingadores.likes} ")
-
 atlanta = Serie('atlanta', 2018, 2)
+panico = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+vingadores.nome = 'vingadores - guerra inifinita parte 2'
+
+
+vingadores.dar_likes()
 atlanta.dar_likes()
-atlanta.dar_likes()
-print(f"{atlanta.nome} - {atlanta.temporadas} : {atlanta.likes} ")
+panico.dar_likes()
+panico.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+filmes_e_series = [vingadores, atlanta, demolidor, panico]
+
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f"\nTamanho da playlist: {len(playlist_fim_de_semana)}\n\nPlaylist:".title())
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+print(f"\nTá ou não está? {demolidor in playlist_fim_de_semana}")
+
+# for programa in filmes_e_series:
+#     detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+#     programa.imprime()
+
---
+++ Python3/OO2/modelo2.py
@@ -0,0 +1,14 @@
+###Abstract base classes
+from abc import ABC
+from collections.abc import MutableSequence
+from numbers import Complex
+
+class Numero(Complex):
+    def __getitem__(self, item):
+        super().__getitem__()
+
+
+filmes = Numero()
+
+
+##Exemplo de collections.
